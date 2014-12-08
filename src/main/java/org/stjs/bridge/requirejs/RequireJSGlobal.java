01242e6f4f3ba068a8f47413c977bad23c32093f
npiguet <npiguet@romandie.com>
2014-12-08 13:14:15 +0100

    Added various overloaded versions of some methods. This allows Java8 style lambdas and method references to work without casting in most cases.

--- src/main/java/org/stjs/bridge/requirejs/RequireJSGlobal.java
+++ src/main/java/org/stjs/bridge/requirejs/RequireJSGlobal.java
@@ -3,10 +3,7 @@ package org.stjs.bridge.requirejs;
 import org.stjs.javascript.Array;
 import org.stjs.javascript.annotation.GlobalScope;
 import org.stjs.javascript.annotation.STJSBridge;
-import org.stjs.javascript.functions.Callback;
-import org.stjs.javascript.functions.Callback0;
-import org.stjs.javascript.functions.Function;
-import org.stjs.javascript.functions.Function0;
+import org.stjs.javascript.functions.*;
 
 @STJSBridge
 @GlobalScope
@@ -17,14 +14,29 @@ public final class RequireJSGlobal {
 
 	public static final RequireJS require = new RequireJS();
 
-	public static native Object define(Function0<Object> moduleConstructor);
+	public static native void define(Function0<?> moduleConstructor);
 
-	public static native Object define(String moduleName, Array<String> dependencies, Function<Object> moduleConstructor);
+	public static native void define(String moduleName, Array<String> dependencies, Function<?> moduleConstructor);
+	public static native void define(String moduleName, Array<String> dependencies, Function0<?> moduleConstructor);
+	public static native void define(String moduleName, Array<String> dependencies, Function1<?, ?> moduleConstructor);
+	public static native void define(String moduleName, Array<String> dependencies, Function2<?, ?, ?> moduleConstructor);
+	public static native void define(String moduleName, Array<String> dependencies, Function3<?, ?, ?, ?> moduleConstructor);
+	public static native void define(String moduleName, Array<String> dependencies, Function4<?, ?, ?, ?, ?> moduleConstructor);
 
-	public static native Object define(Array<String> dependencies, Function<Object> moduleConstructor);
+	public static native void define(Array<String> dependencies, Function<?> moduleConstructor);
+	public static native void define(Array<String> dependencies, Function0<?> moduleConstructor);
+	public static native void define(Array<String> dependencies, Function1<?, ?> moduleConstructor);
+	public static native void define(Array<String> dependencies, Function2<?, ?, ?> moduleConstructor);
+	public static native void define(Array<String> dependencies, Function3<?, ?, ?, ?> moduleConstructor);
+	public static native void define(Array<String> dependencies, Function4<?, ?, ?, ?, ?> moduleConstructor);
 
 	public static native void require(Callback0 moduleConstructor);
 
 	public static native void require(Array<String> dependencies, Callback moduleConstructor);
+	public static native void require(Array<String> dependencies, Callback0 moduleConstructor);
+	public static native void require(Array<String> dependencies, Callback1<?> moduleConstructor);
+	public static native void require(Array<String> dependencies, Callback2<?, ?> moduleConstructor);
+	public static native void require(Array<String> dependencies, Callback3<?, ?, ?> moduleConstructor);
+	public static native void require(Array<String> dependencies, Callback4<?, ?, ?, ?> moduleConstructor);
 
 }
